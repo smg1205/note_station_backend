b3e7081ac23aea5dcf7036da94c0a086f9cb6a86
smg1205 <q3486104707@163.com>
2024-11-19 15:26:01 +0800

    feat: 调试文件预览功能

--- src/main/java/com/smg/NoteStationBackend.java
+++ src/main/java/com/smg/NoteStationBackend.java
@@ -1,5 +1,11 @@
 package com.smg;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class NoteStationBackend {
+    public static void main(String[] args) {
+        SpringApplication.run(NoteStationBackend.class, args);
+    }
 }
